a42978ec11fa966f4f1abcde1045858750e84d06
Ankush Chavan <ankushchavan0411@gmail.com>
2024-04-16 14:00:00 +0530

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 /** @format */
 import { Toaster } from "react-hot-toast";
 import FavoritesQuotes from "./components/FavoritesQuotes";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Quote from "./components/Quote";
 import "./index.css";
@@ -8,10 +9,11 @@ import "./index.css";
 export default function App() {
   return (
     <div className="bg-white">
+      <Toaster position="top-right" reverseOrder={false} />
       <Header />
       <Quote />
       <FavoritesQuotes />
-      <Toaster position="top-right" reverseOrder={false} />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,23 @@
+/** @format */
+
+const Footer = () => {
+  return (
+    <div className="bg-gray-100">
+      <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
+        <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">
+          © 2024 Develop by —
+          <a
+            href="https://www.ankushchavan.in/"
+            rel="noopener noreferrer"
+            className="ml-1 text-blue-600"
+            target="_blank"
+          >
+            Ankush Chavan
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
